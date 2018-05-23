64e9844aac262452729777b4d83fde595952da8e
Corentin Carteau <corentin.carteau@student.ecp.fr>
2018-05-23 08:26:29 +0200

    fix lint MainTable

--- front/src/components/MainTable/MainTable.container.js
+++ front/src/components/MainTable/MainTable.container.js
@@ -2,16 +2,15 @@ import { connect } from 'react-redux';
 
 import MainTable from './MainTable.presentational';
 
-const mapStateToProps = state => {
-    return ({
-        // number of rows to display
-        n_rows: state.selectedNRows,
-        // variable data is displayed
-        variable: state.selectedVariable,
-        // displayed content
-        content: state.data[state.selectedVariable].content,
-        // size of the data, including not displayed data
-        n_total_lines: state.data[state.selectedVariable].n_total_lines
-})};
+const mapStateToProps = state => ({
+  // number of rows to display
+  n_rows: state.selectedNRows,
+  // variable data is displayed
+  variable: state.selectedVariable,
+  // displayed content
+  content: state.data[state.selectedVariable].content,
+  // size of the data, including not displayed data
+  n_total_lines: state.data[state.selectedVariable].n_total_lines,
+});
 
 export default connect(mapStateToProps, null)(MainTable);
